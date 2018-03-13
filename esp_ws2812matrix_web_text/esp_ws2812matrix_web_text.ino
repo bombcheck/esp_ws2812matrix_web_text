fd1fc6ceb35d13dfe4c11022690d83f784f18882
Nils Bernhardt <nils.bernhardt@gmx.net>
2018-03-13 16:08:35 +0100

    Updated details for matrix declaration

--- esp_ws2812matrix_web_text/esp_ws2812matrix_web_text.ino
+++ esp_ws2812matrix_web_text/esp_ws2812matrix_web_text.ino
@@ -10,15 +10,23 @@
 #define PIN 1
 
 // MATRIX DECLARATION:
-// Parameter 1 = width of EACH NEOPIXEL MATRIX (not total display)
-// Parameter 2 = height of each matrix
-  // 8x8 (8, 8)
+// Parameter 1 = width of NeoPixel matrix
+// Parameter 2 = height of matrix
 // Parameter 3 = pin number (most are valid)
-  // PIN 2
-// Parameter 4 = matrix layout flags, add together as needed
+// Parameter 4 = matrix layout flags, add together as needed:
+//   NEO_MATRIX_TOP, NEO_MATRIX_BOTTOM, NEO_MATRIX_LEFT, NEO_MATRIX_RIGHT:
+//     Position of the FIRST LED in the matrix; pick two, e.g.
+//     NEO_MATRIX_TOP + NEO_MATRIX_LEFT for the top-left corner.
+//   NEO_MATRIX_ROWS, NEO_MATRIX_COLUMNS: LEDs are arranged in horizontal
+//     rows or in vertical columns, respectively; pick one or the other.
+//   NEO_MATRIX_PROGRESSIVE, NEO_MATRIX_ZIGZAG: all rows/columns proceed
+//     in the same order, or alternate lines reverse direction; pick one.
+//   See example below for these values in action.
 // Parameter 5 = pixel type flags, add together as needed:
-  // NEO_GRB     Pixels are wired for GRB bitstream (v2 pixels)
-  // NEO_KHZ800  800 KHz bitstream (e.g. High Density LED strip)
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(8, 8, PIN,
   NEO_MATRIX_BOTTOM     + NEO_MATRIX_RIGHT +
   NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG,
